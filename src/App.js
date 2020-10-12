c6dc40a327d3b3823473c6d3bbd5d9878e0152ee
mehrab-haque <mehrab.haque.0001@gmail.com>
2020-10-12 16:44:18 +0600

    auth completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify,{Auth} from 'aws-amplify'
+import awsconfig from './aws-exports'
+import {AmplifySignOut,withAuthenticator} from '@aws-amplify/ui-react'
+
+Amplify.configure(awsconfig)
 
 function App() {
+
+  useEffect(()=>{
+    Auth.currentUserInfo().then(res=>{
+      console.log(res)
+    }).catch(err=>{
+      console.log(err)
+    })
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut/>
+        <h2>My App</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
